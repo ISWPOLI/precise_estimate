203fcb240113cbe3c7d91ddc5dec255b35f5fafa
Sao912 <sak8o12@hotmail.com>
2017-03-02 20:13:01 -0500

    Views project

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProjectService {
+
+  private endpoint_url: string;
+
+  constructor(public http: Http) {
+    this.endpoint_url = "http://localhost:8080/precise_estimate_server/api/project";
+    this.http = http;
+  }
+
+   public createProject(ProjectForm: any) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(this.endpoint_url, ProjectForm, options)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  public listProjects() {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get(this.endpoint_url, options)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  public getProject(id: number) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get(this.endpoint_url + "/" + id, options)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+}
