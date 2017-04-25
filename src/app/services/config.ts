9d1dbb3fd237a1a225649fd9ea94aedc407a9632
Felipe Cano <felipecanol@gmail.com>
2017-04-24 20:10:26 -0500

    Ajuste del config

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -4,6 +4,6 @@ export module ConfigModule {
     * Constante que identifica la URL base para el consumo de servicios REST
     */
     //export const APIURL = 'http://localhost:3000/api';
-    export const APIURL = 'http://server.preciseestimate/api';
+    export const APIURL = 'http://server.preciseestimate.co/api';
 
 }
